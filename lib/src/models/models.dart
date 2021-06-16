7c4f6014a56d110265fc6c70eefae416b45d9d81
anpigon <markan82@gmail.com>
2021-06-16 22:34:15 +0900

    fix export models

--- lib/src/models/models.dart
+++ lib/src/models/models.dart
@@ -1,7 +1,10 @@
 export './block.dart';
+export './asset.dart';
 export './account.dart';
 export './comment.dart';
 export './operation.dart';
+export './rc_params.dart';
+export './rc_account.dart';
 export './transaction.dart';
 export './chain_properties.dart';
 export './vesting_delegation.dart';
